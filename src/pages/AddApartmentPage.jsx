7cf7e70732bfef14a71511163676aa19030d75ac
Natassa <112552395+natko22@users.noreply.github.com>
2023-06-12 12:21:17 +0200

    finished react training for backend

---
+++ src/pages/AddApartmentPage.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+function AddApartmentPage() {
+  const [headline, setHeadline] = useState("");
+  const [price, setPrice] = useState(1);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    // <== HANDLER FUNCTION
+    // Prevent page reload on submit
+    e.preventDefault();
+    // Create the body for the POST request
+    const body = { title: headline, pricePerDay: price };
+
+    axios
+      .post("https://ironbnb-m3.herokuapp.com/apartments", body)
+      .then((response) => {
+        // Reset the state
+        setHeadline("");
+        setPrice(1);
+        // Navigate to the `/` page
+        navigate("/");
+      });
+  };
+
+  return (
+    <div className="AddApartmentPage">
+      <h3>Add New Apartment</h3>
+      <form onSubmit={handleSubmit}>
+        <label>Title</label>
+        <input
+          type="text"
+          name="headline"
+          onChange={(e) => setHeadline(e.target.value)}
+          value={headline}
+        />
+
+        <label>Price per Day</label>
+        <input
+          type="number"
+          name="pricePerDay"
+          onChange={(e) => setPrice(e.target.value)}
+          value={price}
+        />
+
+        <button type="submit">Create Apartment</button>
+      </form>
+    </div>
+  );
+}
+
+export default AddApartmentPage;
